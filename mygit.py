dc7fd98924afe4a751dcdecb76a60297599f2681
vlad6711 <asafevv0905@gmail.com>
2025-03-05 11:37:36 +0300

    added documentation

--- mygit.py
+++ mygit.py
@@ -3,5 +3,5 @@ a=3
 b=2
 print("sum", a+b)
 print("dev", a-b)
-print("mul", a*b
+print("mul", a*b)
 print("div", a/b)
---
+++ readme.md
@@ -0,0 +1,5 @@
+# this is the first program
+## this is the first section
+### first paragraf
+
+this is my main code
